ee62d31fc826095c0fc05035add13ddde93391ac
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-10-17 15:30:42 -0400

    removed debug condition to staticfiles dirs

--- myprofile/settings.py
+++ myprofile/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'frontend/public']
+
+STATICFILES_DIRS = [BASE_DIR / 'frontend/public']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
