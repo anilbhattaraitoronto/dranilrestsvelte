8937c89b68568b3000222208395cd16e388c8e16
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-10-19 16:15:25 -0400

    revamped the post list pages - added card footer and pushed the posted date to the top of card content

--- frontend/src/components/pages/Home.svelte
+++ frontend/src/components/pages/Home.svelte
@@ -22,45 +22,51 @@
             <h2 class="title has-text-centered">Featured Posts</h2>
             <div class="posts container ">
                 {#each $featuredPosts as post}
-                    <div class="columns m-2 p-2 card">
-                        {#if post.thumbnail}
-                            <figure class="column is-one-fifth">
-                                <img
-                                    src={post.thumbnail}
-                                    alt=""
-                                    class="card-image" />
-                            </figure>
-                        {/if}
-                        <div class="column card-content">
-                            <h3 class="subtitle">
-                                <a
-                                    href="#/posts/{post.category.slug}">{post.category.name}</a>
-                            </h3>
-                            <h2 class="title">
-                                <a
-                                    href="#/posts/{post.id}/{post.slug}">{post.title}</a>
-                            </h2>
+                    <div class="card my-2 p-1">
+                        <div class="columns  is-mobile">
+                            {#if post.thumbnail}
+                                <figure class="column is-one-fourth">
+                                    <img
+                                        src={post.thumbnail}
+                                        alt=""
+                                        class="card-image" />
+                                </figure>
+                            {/if}
+                            <div class="column card-content">
+                                <p>
+                                    <span
+                                        class="is-size-7 is-italic">{new Date(post.posted_date).toDateString()}</span>
+                                </p>
+                                <h3 class="is-size-6 is-italic">
+                                    <a
+                                        href="#/posts/{post.category.slug}"
+                                        class="">{post.category.name}</a>
+                                </h3>
+                                <h2 class="is-size-4">
+                                    <a
+                                        href="#/posts/{post.id}/{post.slug}">{post.title}</a>
+                                </h2>
 
-                            <p class="is-size-5 is-italic my-6">
-                                {post.summary}
-                            </p>
-                            <p class="has-text-right my-3">
-                                <a
-                                    href="#/posts/{post.id}/{post.slug}"
-                                    class="button is-outlined is-link">Read full</a>
-                            </p>
-                            <p class="has-text-right my-6">
-                                <em>Topics: </em>
-                                {#each post.tags as tag}
-                                    <span class="button is-small">
-                                        <a
-                                            href="#/tagposts/{tag.slug}">{tag.name}</a></span>
-                                {/each}
-                            </p>
-                            <p class="is-italic is-size-7 has-text-weight-bold">
-                                {new Date(post.posted_date).toDateString()}
-                            </p>
+                                <p class="is-size-5 is-italic my-1">
+                                    {post.summary.substring(0, 50)}
+                                    ...
+                                </p>
+                                <p class="has-text-right my-1">
+                                    <a
+                                        href="#/posts/{post.id}/{post.slug}"
+                                        class="button is-outlined is-link">Read
+                                        full</a>
+                                </p>
+                            </div>
                         </div>
+                        <footer class="card-footer">
+                            <em class="card-footer-item">Topics: </em>
+                            {#each post.tags as tag}
+                                <span class="card-footer-item">
+                                    <a
+                                        href="#/tagposts/{tag.slug}">{tag.name}</a></span>
+                            {/each}
+                        </footer>
                     </div>
                 {/each}
             </div>
--- frontend/src/components/pages/TagPosts.svelte
+++ frontend/src/components/pages/TagPosts.svelte
@@ -27,7 +27,7 @@
         {#if $tagPosts.length > 0}
             <div class="posts">
                 {#each $tagPosts as post}
-                    <div class=" columns post card m-2 p-2">
+                    <!-- <div class=" columns post card m-2 p-2">
                         {#if post.thumbnail}
                             <figure class="column is-one-fifth">
                                 <img
@@ -66,6 +66,52 @@
                                 {new Date(post.posted_date).toDateString()}
                             </p>
                         </div>
+                    </div> -->
+                    <div class="card my-2 p-1">
+                        <div class="columns  is-mobile">
+                            {#if post.thumbnail}
+                                <figure class="column is-one-fourth">
+                                    <img
+                                        src={post.thumbnail}
+                                        alt=""
+                                        class="card-image" />
+                                </figure>
+                            {/if}
+                            <div class="column card-content">
+                                <p>
+                                    <span
+                                        class="is-size-7 is-italic">{new Date(post.posted_date).toDateString()}</span>
+                                </p>
+                                <h3 class="is-size-6 is-italic">
+                                    <a
+                                        href="#/posts/{post.category.slug}"
+                                        class="">{post.category.name}</a>
+                                </h3>
+                                <h2 class="is-size-4">
+                                    <a
+                                        href="#/posts/{post.id}/{post.slug}">{post.title}</a>
+                                </h2>
+
+                                <p class="is-size-5 is-italic my-1">
+                                    {post.summary.substring(0, 50)}
+                                    ...
+                                </p>
+                                <p class="has-text-right my-1">
+                                    <a
+                                        href="#/posts/{post.id}/{post.slug}"
+                                        class="button is-outlined is-link">Read
+                                        full</a>
+                                </p>
+                            </div>
+                        </div>
+                        <footer class="card-footer">
+                            <em class="card-footer-item">Topics: </em>
+                            {#each post.tags as tag}
+                                <span class="card-footer-item">
+                                    <a
+                                        href="#/tagposts/{tag.slug}">{tag.name}</a></span>
+                            {/each}
+                        </footer>
                     </div>
                 {/each}
             </div>
