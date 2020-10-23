1812b23d153dd9df609e8403daf2cb7e72ad906f
Anil Bhattarai <anil.bhattarai@mail.utoronto.ca>
2020-10-23 18:36:48 -0400

    added hero container for featured posts in homepage with black background

--- frontend/src/components/pages/Home.svelte
+++ frontend/src/components/pages/Home.svelte
@@ -9,86 +9,82 @@
     <title>Welcome to Dr Anil Bhattarai's Page</title>
 </svelte:head>
 
-<main>
-    <!-- <div class="container card">
-        <div class="card-image is-3by4">
-            <figure class="image" transition:fade>
-                <img src={homeImage} alt="" />
-            </figure>
-        </div>
-    </div> -->
+<article class="container m-0">
+    <div class="hero is-black">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h2 class="title is-2">Welcome</h2>
+                <p class="has-text-warning-dark">A new world awaits, always!</p>
+                {#if $featuredPosts.length > 0}
+                    <div class="posts container ">
+                        <h2 class="title has-text-centered my-4">
+                            Featured Posts
+                        </h2>
+                        {#each $featuredPosts as post}
+                            <div class="card my-2">
+                                <div class=" is-mobile">
+                                    {#if post.imageurl}
+                                        <div class="card-image">
+                                            <figure
+                                                class="image"
+                                                transition:fade>
+                                                <img
+                                                    src={post.imageurl}
+                                                    alt="" />
+                                            </figure>
+                                        </div>
+                                    {:else if post.image}
+                                        <div class="card-image ">
+                                            <figure
+                                                class="image"
+                                                transition:fade>
+                                                <img src={post.image} alt="" />
+                                            </figure>
+                                        </div>
+                                    {/if}
+                                    <div class=" card-content">
+                                        <p>
+                                            <span
+                                                class="is-size-7 is-italic">{new Date(post.posted_date).toDateString()}</span>
+                                        </p>
+                                        <h3 class="is-size-6 is-italic">
+                                            <a
+                                                href="#/posts/{post.category.slug}">{post.category.name}</a>
+                                        </h3>
+                                        <h2 class="is-size-4 title">
+                                            <a
+                                                href="#/posts/{post.id}/{post.slug}"
+                                                class="has-text-black">{post.title}</a>
+                                        </h2>
 
-    <article class="container">
-        <div class="hero is-light">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h2 class="title is-2">Welcome</h2>
-                    <p class="has-text-danger-dark">
-                        A new world awaits, always!
-                    </p>
-                </div>
-            </div>
-        </div>
-        {#if $featuredPosts.length > 0}
-            <h2 class="title has-text-centered my-4">Featured Posts</h2>
-            <div class="posts container ">
-                {#each $featuredPosts as post}
-                    <div class="card my-2 p-1">
-                        <div class=" is-mobile">
-                            {#if post.imageurl}
-                                <div class="card-image">
-                                    <figure class="image" transition:fade>
-                                        <img src={post.imageurl} alt="" />
-                                    </figure>
-                                </div>
-                            {:else if post.image}
-                                <div class="card-image ">
-                                    <figure class="image" transition:fade>
-                                        <img src={post.image} alt="" />
-                                    </figure>
-                                </div>
-                            {/if}
-                            <div class=" card-content">
-                                <p>
-                                    <span
-                                        class="is-size-7 is-italic">{new Date(post.posted_date).toDateString()}</span>
-                                </p>
-                                <h3 class="is-size-6 is-italic">
-                                    <a
-                                        href="#/posts/{post.category.slug}">{post.category.name}</a>
-                                </h3>
-                                <h2 class="is-size-4 title">
-                                    <a
-                                        href="#/posts/{post.id}/{post.slug}"
-                                        class="has-text-black">{post.title}</a>
-                                </h2>
-
-                                <p
-                                    class="is-size-5 is-italic my-6 has-background-light p-2">
-                                    {post.summary}
-                                    ...
-                                    <!-- <a
+                                        <p
+                                            class="is-size-5 is-italic my-6 has-background-light p-2">
+                                            {post.summary}
+                                            ...
+                                            <!-- <a
                                         href="#/posts/{post.id}/{post.slug}"
                                         class="is-link">Read full</a> -->
-                                </p>
-                                <div class="content">
-                                    {@html post.content}
+                                        </p>
+                                        <div class="content">
+                                            {@html post.content}
+                                        </div>
+                                    </div>
                                 </div>
+                                <footer class="card-footer">
+                                    <em class="card-footer-item">Topics: </em>
+                                    {#each post.tags as tag}
+                                        <span class="card-footer-item">
+                                            <a
+                                                href="#/tagposts/{tag.slug}">{tag.name}</a></span>
+                                    {/each}
+                                </footer>
                             </div>
-                        </div>
-                        <footer class="card-footer">
-                            <em class="card-footer-item">Topics: </em>
-                            {#each post.tags as tag}
-                                <span class="card-footer-item">
-                                    <a
-                                        href="#/tagposts/{tag.slug}">{tag.name}</a></span>
-                            {/each}
-                        </footer>
+                        {/each}
                     </div>
-                {/each}
+                {:else}
+                    <p>No Posts yet</p>
+                {/if}
             </div>
-        {:else}
-            <p>No Posts yet</p>
-        {/if}
-    </article>
-</main>
+        </div>
+    </div>
+</article>
